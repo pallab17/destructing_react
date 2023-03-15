b626b4b3db95178b64ca8b79ab7eaaf620605b4e
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-15 14:30:25 +0530

    Update basics.js

--- src/basics.js
+++ src/basics.js
@@ -1,1 +1,18 @@
 
+// import animals, { useAnimals } from "./data";
+
+// //Destructuring Arrays
+// // console.log(animals);
+// const [cat, dog] = animals;
+// // console.log(cat);
+
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+
+// //Destructuring Objects
+// // const { name, sound} = cat;
+// // const { name: catName, sound: catSound } = cat;
+// // const { name = "Fluffy", sound = "Purr" } = cat;
+// // const {feedingRequirements: {food, water} } = cat;
+// // console.log(food);
